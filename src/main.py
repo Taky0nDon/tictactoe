0616e644c2aa7c02792d2d532b19c978a496e9c1
Taky0ndon <char@za.rd>
2024-04-03 22:57:15 -0400

    added some type annotations, and checked for winning condition (rows only)

--- src/main.py
+++ src/main.py
@@ -1,40 +1,53 @@
 class BoardState:
     def __init__(self):
-        self.top_row    = [" ", " ", " "]
-        self.middle_row = [" ", " ", " "]
-        self.bottom_row = [" ", " ", " "]
+        self.top_row: list[str]    = [" ", " ", " "]
+        self.middle_row: list[str] = [" ", " ", " "]
+        self.bottom_row: list[str] = [" ", " ", " "]
 
-        self.POSITION_HASH = {
+        self.POSITION_HASH: dict[str, list[str]] = {
             "A": self.top_row,
             "B": self.middle_row,
             "C": self.bottom_row
                     }
 
-    def update_board_state(self, position: str, player: str):
+    def get_board_state(self) -> dict[str, list[str]]:
+        """ Return the dictionary containing up to date board status (position of places X and Os) """
+        return self.POSITION_HASH
+
+    def update_board_state(self, position: str, player: str) -> None:
         """
-:param str position: The position to change, in the form of A1, where the first character is A, B
-or C, and the second character is 1, 2, or 3. The first character represents the row to alter, and the second character the column.
-for Example, a position of "A1" will add an X or an O to the upper-left space.
+    @param position: The position to change, in the form of A1, where the first character is A, B
+    or C, and the second character is 1, 2, or 3. The first character represents the row to alter, and the second character the column.
+    for Example, a position of "A1" will add an X or an O to the upper-left space.
+    @param player: The symbol to place on the board (X or O)
         """
-        row = position[0].upper()
-        column = int(position[1]) - 1
+        row: str = position[0].upper()
+        column: int = int(position[1]) - 1
         self.POSITION_HASH[row][column] = player
 
-    def move_is_valid(self, position) -> bool:
-        if self.POSITION_HASH[position[0].upper()][int(position[1]) - 1] != " "\
-        or position[0].upper() not in "ABC"\
-        or int(position[1]) not in range(1, 4):
+    def move_is_valid(self, position: str) -> bool:
+        row: str = position[0].upper()
+        column: int = int(position[1]) - 1
+        if self.POSITION_HASH[row][column] != " "\
+        or row not in "ABC"\
+        or column not in range(0, 3):
             return False
         else:
             return True
 
+    def game_is_over(self) -> bool:
+        state = self.get_board_state()
+        rows = state.values()
+        for row in rows:
+            if any([column != " " for column in row]) and len(set(row)) == 1:
+                return True
 
 class Player:
-    def __init__(self, symbol):
+    def __init__(self, symbol:str):
         self.symbol = symbol
 
-    def take_turn(self):
-        position = input("Enter the coordinates for you next move")
+    def take_turn(self) -> str:
+        position = input("Enter the coordinates for you next move: ")
         return position
 
 
