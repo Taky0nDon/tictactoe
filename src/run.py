8b14ef919b8f5cdfb8d44d1e18786334a3bbf9f2
Taky0ndon <char@za.rd>
2024-04-06 20:45:18 -0400

    check for almost won rows and have CPU block them

--- src/run.py
+++ src/run.py
@@ -1,18 +1,22 @@
+from os import system
 from random import randint, choice
-from main import BoardState, BoardMaker, Player, CPUOpponent
+
+from main import BoardState, BoardMaker, PLAYER_SYMBOLS
+from Player import Player, CPUOpponent
 
 
 BoardMaker = BoardMaker()
 game_state = BoardState()
 
-symbols = ["x", "o"]
-
-player = Player(symbols.pop(randint(0,1)))
-cpu_player = CPUOpponent(symbols.pop(0))
+player = Player(PLAYER_SYMBOLS.pop(randint(0,1)))
+cpu_player = CPUOpponent(PLAYER_SYMBOLS.pop(0))
 current_player = player
 other_player = cpu_player
+draw = False
 
 while not game_state.game_is_over():
+    print(f'{game_state.POSITION_HASH.values()=}')
+    print(f'{other_player.symbol=}')
     if current_player.is_cpu == False:
         BoardMaker.make_board(game_state)
         print(f"{current_player.symbol}, it's your turn. Go!")
@@ -20,16 +24,26 @@ while not game_state.game_is_over():
         if not game_state.move_is_valid(move):
             print("Sorry, that move is not valid.")
             continue
-        cpu_player.open_positions.remove(move.upper())
+# TODO: put this checking logic in BoardState then pass it to the CPUOpponent
+    elif any([row.count(other_player.symbol) == 2
+              and row.count(" ") == 1 for row in game_state.POSITION_HASH.values()]):
+        print('here')
+        move = current_player.block_win(game_state)
     else:
-        move = choice(cpu_player.open_positions)
+        move = choice(game_state.open_positions)
+    print(f'{move=}')
     game_state.update_board_state(position=move, player=current_player.symbol)
+    game_state.open_positions.remove(move.upper())
+    # system("clear")
     if not game_state.game_is_over():
         current_player, other_player = other_player, current_player
-    if len(cpu_player.open_positions) < 1:
-        game_state.game_is_over("true")
-        print("It's a draw!")
+    if len(game_state.open_positions) < 1:
+        draw = True
+
 
 BoardMaker.make_board(game_state)
-print(f"Game over! {current_player.symbol.upper()} won!")
+if draw:
+    print("It's a draw!")
+else:
+    print(f"Game over! {current_player.symbol.upper()} won!")
 
