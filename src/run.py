777427f2f589208cfbaf76dce4f14a5e27d8a335
Taky0ndon <char@za.rd>
2024-04-10 19:11:39 -0400

    added check for almost won diagonals and cpu method for blocking them

--- src/run.py
+++ src/run.py
@@ -12,39 +12,42 @@ player = Player(PLAYER_SYMBOLS.pop(randint(0,1)))
 cpu_player = CPUOpponent(PLAYER_SYMBOLS.pop(0))
 current_player = player
 other_player = cpu_player
-draw = False
-print(len(game_state.open_positions))
+DRAW = False
 while not game_state.game_is_won() and not game_state.game_is_draw():
-    print(f'{game_state.game_is_won()=}, {game_state.game_is_draw()=}')
-    print(f'{game_state.get_columns()=}')
-    print(f'{game_state.POSITION_HASH.values()=}')
-    print(f'{other_player.symbol=}')
-    if current_player.is_cpu == False:
+# TODO: put this checking logic in BoardState then pass it to the CPUOpponent
+    row_almost_won = any(row.count(other_player.symbol) == 2 and\
+            row.count(" ") == 1 for row in game_state.get_rows())
+    column_almost_won = any(col.count(other_player.symbol) == 2 and\
+            col.count(" ") == 1 for col in game_state.get_columns())
+    diag_almost_won = any(diag.count(other_player.symbol) == 2 and\
+            diag.count(" ") == 1 for diag in game_state.get_diagonals())
+
+    if current_player.is_cpu is False:
         BoardMaker.make_board(game_state)
         print(f"{current_player.symbol}, it's your turn. Go!")
         move = input("Enter the coordinates for your move: ")
         if not game_state.move_is_valid(move):
             print("Sorry, that move is not valid.")
             continue
-# TODO: put this checking logic in BoardState then pass it to the CPUOpponent
-    elif any([row.count(other_player.symbol) == 2
-              and row.count(" ") == 1 for row in game_state.POSITION_HASH.values()]):
-        print('here')
-        move = current_player.block_win(game_state)
+    elif diag_almost_won:
+        move = current_player.block_diagonal_win(game_state)
+    elif row_almost_won:
+        move = current_player.block_row_win(game_state)
+    elif column_almost_won:
+        print("column almost won")
+        move = choice(game_state.open_positions)
     else:
         move = choice(game_state.open_positions)
-    print(f'{move=}')
     game_state.update_board_state(position=move, player=current_player.symbol)
     game_state.open_positions.remove(move.upper())
 #    system("clear")
-    draw = game_state.game_is_draw()
-    if not game_state.game_is_won() and not draw:
+    DRAW = game_state.game_is_draw()
+    if not game_state.game_is_won() and not DRAW:
         current_player, other_player = other_player, current_player
 
 
 BoardMaker.make_board(game_state)
-if draw:
+if DRAW:
     print("It's a draw!")
 else:
     print(f"Game over! {current_player.symbol.upper()} won!")
-
